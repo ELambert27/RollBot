1b839884da6630ac2179fa751ada7c183b8725ab
JLambert27 <alex27lambert@yahoo.com>
2017-11-25 20:19:57 -0500

    Roll d20
    Added the /roll command, allowing rollbot to roll a d20. Also updated
    the README and the bot name is package.json

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RollBot
 Discord bot for rolling dice
+
+Rollbot is a bot that generates random numbers to replicate rolling dice. With version 1.0, the system is going to generate D&D-style dice numbers (1d20, 2d6, etc), and should be able to read any kind of random number through the use of regular expressions (hopefully).
+
+If you want to copy and use your own rollbot, copy the files, add a file called auth.json with the following text:
+	{
+		"token": "YOUR_TOKEN_HERE"
+	}
+I'd recommend also following the instructions at this link ( https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token ) to get starting with creating a bot account and running the bot.
--- bot.js
+++ bot.js
@@ -14,26 +14,36 @@ var bot = new Discord.Client({
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
-    logger.info('Logged in as: ');
+    logger.info('Logged in as: bot.username');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == '/') {
         var args = message.substring(1).split(' ');
-        var cmd = args[0];
+        var cmd = message.substring(1, 5);
+	var remainder = message.substring(5);
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
+            // /roll
+            case 'roll':
+		if (remainder.length > 0) {
+			bot.sendMessage({
+			    to: channelID,
+			    message: "I can't do this yet"
+			});
+		}
+		else { //remainder.length <= 0
+			var randomNumber = Math.ceil(Math.random() * 20);
+        	        bot.sendMessage({
+        	            to: channelID,
+                	    message: randomNumber
+	                });
+		}
+                break;
+
          }
      }
 });
